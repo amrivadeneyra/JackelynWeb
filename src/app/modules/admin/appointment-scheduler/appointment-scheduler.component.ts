16ea563334cfdcdb943e69a23c78f16036dc36e7
manuel <arivadeneyrai@autonoma.edu.pe>
2023-11-19 23:30:51 -0500

    reserva de citas, modelo, estilos, diseño de la ruta para reserva de citas

--- src/app/modules/admin/appointment-scheduler/appointment-scheduler.component.ts
+++ src/app/modules/admin/appointment-scheduler/appointment-scheduler.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Service } from 'src/app/models/service';
 import { servicesValue } from 'src/app/values/service';
 
@@ -11,8 +12,48 @@ export class AppointmentSchedulerComponent {
 
   services: Service[] = [];
 
+  appointmentSchedulerForm!: UntypedFormGroup;
+
+  constructor(
+    private fb: UntypedFormBuilder,
+  ) { }
+
   ngOnInit(): void {
     this.services = servicesValue;
+    this.initAppointmentSchedulerForm();
+  }
+
+  resetDate(): void { }
+
+  initAppointmentSchedulerForm(): void {
+    this.appointmentSchedulerForm = this.fb.group({
+      _id: [''],
+      client_name: ['', Validators.required],
+      phone_number: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: [''],
+      emergency_contact: [''],
+
+      service_name: ['', Validators.required],
+      price: [0, Validators.required],
+      description: [''],
+      category: [0, Validators.required],
+
+      appointment_date: [new Date(), Validators.required],
+      appointment_time: ['', Validators.required],
+
+      payment_method: [''],
+      payment_status: [''],
+
+      duration: [0],
+      recurrence: [''],
+      notes: [''],
+
+      notification_preferences: [[]],
+
+      created_at: [new Date()],
+      updated_at: [new Date()],
+    })
   }
 
 }
