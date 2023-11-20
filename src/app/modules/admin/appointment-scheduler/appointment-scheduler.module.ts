09e4b98973f1a0882f569fd79292f02aca8ee70f
manuel <arivadeneyrai@autonoma.edu.pe>
2023-11-19 19:41:33 -0500

    a

--- src/app/modules/admin/appointment-scheduler/appointment-scheduler.module.ts
+++ src/app/modules/admin/appointment-scheduler/appointment-scheduler.module.ts
@@ -4,8 +4,6 @@ import { RouterModule } from "@angular/router";
 import { appointmentSchedulerRoutes } from "./appointment-scheduler.routing";
 import { SidenavModule } from "src/app/layouts/sidenav/sidenav.module";
 import { CommonModule } from "@angular/common";
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
